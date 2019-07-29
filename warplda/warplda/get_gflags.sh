c972331d8d0655b580781206fef41436df4cb479
abhishekpanigrahi <abhishekpanigrahi034@gmail.com>
2019-07-29 19:41:34 +0000

    Removed warplda

--- warplda/warplda/get_gflags.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/gflags/gflags.git
-cd gflags
-git checkout d701ceac73be2c43b6e7b97474184e626fded88b
--- warplda/warplda/src/lda.cpp
+++
@@ -1,8 +0,0 @@
-#include "lda.hpp"
-
-void LDA::loadBinary(std::string fname)
-{
-    if (!g.Load(fname))
-        throw std::runtime_error(std::string("Load Binary failed : ") + fname);
-	printf("Bigraph loaded from %s, %u documents, %u unique tokens, %lu total words\n", fname.c_str(), g.NU(), g.NV(), g.NE());
-}
